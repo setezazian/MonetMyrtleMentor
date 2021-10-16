5e1f93fa5b8b8688a5efc904977fdc40334fc13c
FaceToKeyboard <fanno.chea@gmail.com>
2021-10-16 16:10:56 -0600

    express middlewares

--- server/app.js
+++ server/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
-const PORT = 3000;
+
+// Handle JSON and query strings
+app.use(express.json());
+app.use(express.urlencoded({ urlencoded: true }));
+
+// Serve site/client from root path
+app.use(express.static(path.resolve('client', 'dist')));
 
 // Router
 const router = require('./routes');
 
 // Set up routes
-app.use('/', router);
+app.use('/api', router);
 
 module.exports = app;
