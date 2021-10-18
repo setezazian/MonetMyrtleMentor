c66bff9476b2a00a7cf47c676d92c734fd1c297c
Shirin <shirinetezazian@gmail.com>
2021-10-18 11:11:33 -0600

    Update apiRouter to only handle routing to the controlller

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -1,31 +1,10 @@
 const router = require('express').Router();
 const { getActivities, getProfile, getMessages } = require('./controller');
 
-router.get('/activities', (req, res) => {
-  // get activities from database, finalize shape, respond
-  getActivities()
-    .then((dbData) => {
-      res.status(200).send(dbData);
-    })
-    .catch(err => console.log('Error retrieving from database: ', err));
-});
+router.get('/activities', getActivities);
 
-router.get('/profile', (req, res) => {
-  // get and finalize data, finalize shape, respond
-  getProfile()
-    .then((dbData) => {
-      res.status(200).send(dbData);
-    })
-    .catch(err => console.log('Error retrieving from database: ', err));
-});
+router.get('/profile', getProfile);
 
-router.get('/messages', (req, res) => {
-  // get and finalize data, finalize shape, respond
-  getMessages()
-    .then((dbData) => {
-      res.status(200).send(dbData);
-    })
-    .catch(err => console.log('Error retrieving from database: ', err));
-});
+router.get('/messages', getMessages);
 
 module.exports = router;
