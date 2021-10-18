a6b02d5ffa7ed4a8cb339ce0fdb4a4ca83a0c018
Shirin <shirinetezazian@gmail.com>
2021-10-18 15:39:32 -0600

    connect controller to models

---
+++ server/controller.js
@@ -0,0 +1,37 @@
+const profileModel = require('./models/profile.js');
+const offeringsModel = require('./models/offerings.js');
+const messagesModel = require('./models/messages.js');
+
+const getOfferings = (req, res) => {
+  // Read req params into vars
+  // const page = 1;
+  // const count = 10;
+  offeringsModel.getOfferings() // (count, page)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => console.log('Error retrieving offerings from model: ', err));
+};
+
+const getProfile = (req, res) => {
+  const profileId = 1;
+  profileModel.getProfile(profileId)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => console.log('Error retrieving profile from model: ', err));
+};
+
+const getMessages = (req, res) => {
+  messagesModel.getMessages() // (userId, withId = null, page = 1, count = 10)
+    .then((data) => {
+      res.status(200).send(data);
+    })
+    .catch((err) => console.log('Error retrieving messages from model: ', err));
+};
+
+module.exports = {
+  getOfferings,
+  getProfile,
+  getMessages,
+};
