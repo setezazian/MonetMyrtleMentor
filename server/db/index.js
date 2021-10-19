4382d0c99874103aa93ace107f87e29186ccab77
FaceToKeyboard <fanno.chea@gmail.com>
2021-10-19 13:25:00 -0600

    use variables to configure DB connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,16 +1,16 @@
 const dbConnector = require('mysql');
 
 const connection = dbConnector.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'mentorUp',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || 3306,
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'password',
+  database: process.env.DB_NAME || 'mentorUp',
 });
 
 connection.connect((err) => {
   if (err) {
-    console.log('error connecting');
+    console.log('error connecting to database');
   } else {
     console.log('sucessfully connected to MySQL!');
   }
