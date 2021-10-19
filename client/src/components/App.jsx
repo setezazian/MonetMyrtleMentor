7cc9cfc6ea9907c3e1986c586ea9696a98a2b8ba
FaceToKeyboard <fanno.chea@gmail.com>
2021-10-19 12:25:42 -0600

    add react route to signup component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,31 +1,33 @@
-import React, {useState, createContext} from 'react';
-import LandingPage from './LandingPage/LandingPage.jsx';
+import React, { useState, createContext } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './LandingPage/LandingPage.jsx';
 import Offerings from './offeringPage/Offerings.jsx';
 import Navbar from './Navbar.jsx';
 import Profile from './Profile/Profile.jsx';
+import FormSignup from './FormSignup/FormSignup.jsx';
 
 const pageIdxContext = React.createContext();
 const App = () => {
   const [pageIdx, setPageIdx] = useState(-1);
   return (
-      // <div>
-      //   Loading...
-      // </div>
-        <pageIdxContext.Provider value={{pageIdx, setPageIdx}}>
-          <div>
-              <BrowserRouter>
-              <Navbar />
-                <Switch>
-                  <Route exact path="/" component={LandingPage} />
-                  <Route path="/offerings" component={Offerings} />
-                  <Route path="/profile" component={Profile} />
-                </Switch>
-              </BrowserRouter>
-          </div>
-        </pageIdxContext.Provider>
-  )
-}
+  // <div>
+  //   Loading...
+  // </div>
+    <pageIdxContext.Provider value={{ pageIdx, setPageIdx }}>
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/offerings" component={Offerings} />
+            <Route path="/profile" component={Profile} />
+            <Route exact path="/signup" component={FormSignup} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </pageIdxContext.Provider>
+  );
+};
 
 export default App;
-export { pageIdxContext }
+export { pageIdxContext };
