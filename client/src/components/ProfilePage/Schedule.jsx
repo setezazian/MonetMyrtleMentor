37111a50dc15f07efdf195a8d4be9cacbf4f1dca
Shirin <shirinetezazian@gmail.com>
2021-10-20 11:26:50 -0600

    Implement schedule to show availabilities and add react-calender styling

--- client/src/components/ProfilePage/Schedule.jsx
+++ client/src/components/ProfilePage/Schedule.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
 import axios from 'axios';
+import 'react-calendar/dist/Calendar.css';
 
 function Schedule() {
-  const [date, setDate] = useState(new Date());
+  const [date, setDate] = useState(null);
+  const [availability, setAvailability] = useState(null);
 
   useEffect(() => {
     axios.get('/api/schedule', {
@@ -13,22 +15,28 @@ function Schedule() {
     })
       .then((response) => {
         console.log(response);
-        // setDate(response.data[0]);
+        if (typeof (response.data) === 'object') {
+          setAvailability(response.data);
+        }
       })
       .catch((error) => {
         console.log(error);
       });
-  });
+  }, [date]);
 
   return (
-    <div>
+    <div className="scheduling">
       <div className="calendar">
         <Calendar
           onChange={setDate}
           value={date}
         />
       </div>
-      <div>{date.toString()}</div>
+      <div id="schedule" className={date === null ? 'hidden' : undefined}>
+        {availability && JSON.stringify(availability[0].start_time)}
+        {availability && JSON.stringify(availability[0].end_time)}
+      </div>
+      {/* <div>{date.toString()}</div> */}
     </div>
   );
 }
