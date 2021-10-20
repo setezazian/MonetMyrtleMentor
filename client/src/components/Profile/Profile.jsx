3c26c09231cd4ca2d97f7e882b6a5df42e2a6b6d
Jason-K-87 <jason.kenan.west@gmail.com>
2021-10-20 08:27:27 -0600

    Added messages component, set up styles and route to get messages from database

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Messages from './Messages.jsx';
 
 export default function Profile() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/messages')
+      .then((res) => {
+        setMessages(res.data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
   return (
-    <h1>Profile page</h1>
-  )
+    <div>
+      <h1>Profile page</h1>
+      <div className="messages-wrapper">
+        {messages.map((element) => (
+          <Messages
+            key={element.id}
+            from={element.from_id}
+            to={element.to_id}
+            body={element.body}
+            time={element.time}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
