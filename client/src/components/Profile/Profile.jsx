e2866bcb1db8920ebf6a434f222acdcca8ee0e2c
louis09392003 <louis09392003@gmail.com>
2021-10-22 10:12:10 -0600

    log out func

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -2,11 +2,26 @@ import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
 import Messages from './Messages.jsx';
 import Schedule from './Schedule.jsx';
-import { loginProfileContext } from '../../context.jsx';
+import pageIdxContext, { loginContext, loginProfileContext } from '../../context.jsx';
+import { useHistory } from 'react-router-dom';
 
 export default function Profile() {
   const [messages, setMessages] = useState([]);
-  const { loginIdx } = useContext(loginProfileContext);
+  const { setPageIdx } = useContext(pageIdxContext);
+  const { loginIdx, setLoginIdx } = useContext(loginProfileContext);
+  const { setLogin } = useContext(loginContext);
+  const history = useHistory();
+
+  const logout = (e) => {
+    e.preventDefault();
+    axios.get('/api/logout')
+      .then(() => {
+        setLogin(false);
+        setLoginIdx(-1);
+        setPageIdx(0);
+        history.push('/');
+      });
+  };
 
   useEffect(() => {
     axios.post('/api/getMessages', { loginIdx })
@@ -47,6 +62,7 @@ export default function Profile() {
     <div>
       <div className="enrollmentTitle">
         Personal Profile
+        <button className="logout-btn" onClick={logout}>Log out</button>
       </div>
       <Schedule profileId={loginIdx} />
       {conditionalReturn()}
