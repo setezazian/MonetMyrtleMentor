ad2bd76a69bec41f09a0f34f2f34481ff119b4b4
Shirin <shirinetezazian@gmail.com>
2021-10-21 22:12:47 -0600

    Add routes to api router

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -11,6 +11,8 @@ const {
   getSchedule,
   searchOfferings,
   goodAuth,
+  createBooking,
+  getProfileSchedule,
 } = require('./controller');
 
 router.get('/offerings', getOfferings);
@@ -32,6 +34,11 @@ router.post('/messages', postMessage);
 router.get('/schedule', getSchedule);
 
 router.post('/user/new', createNewUser);
+router.post('/booking', createBooking);
+
+router.get('/profile/schedule', getProfileSchedule);
+
+router.post('/user/new', createAuthUser);
 
 router.post('/user/login', passport.authenticate('local',
   {
